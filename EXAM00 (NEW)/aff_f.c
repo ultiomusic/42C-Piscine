b19b85e3d36e15e6c25935e24ff27194a0f11a49
Ultio <94832543+ultiomusic@users.noreply.github.com>
2023-03-24 02:38:22 +0300

    Delete EXAM00 (NEW) directory

--- EXAM00 (NEW)/aff_f.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-
-int		main(int ac, char **av)
-{
-	(void)ac;
-	(void)av;
-	write(1, "f\n", 2);
-	return (0);
-}
--- EXAM00 (NEW)/double_figures.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-
-int		main(void)
-{
-	write(1, "0 2 4 6 8 10", 12);
-	return (0);
-}
