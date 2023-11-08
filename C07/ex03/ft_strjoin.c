f687a62d1e31767f206702031d832417debeb5ae
Ultio <94832543+ultiomusic@users.noreply.github.com>
2023-11-08 21:35:23 +0300

    Update ft_strjoin.c

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -1,70 +1,29 @@
 #include <stdlib.h>
-int	ft_str_length(char *str)
-{
-	int	index;
-
-	index = 0;
-	while (str[index])
-		index++;
-	return (index);
-}
-
-char	*ft_strcpy(char *dest, char *src)
-{
-	int	index;
-
-	index = 0;
-	while (src[index] != '\0')
-	{
-		dest[index] = src[index];
-		index++;
-	}
-	dest[index] = '\0';
-	return (dest);
-}
-
-int	ft_compute_final_length(char **strings, int size, int sep_length)
-{
-	int	final_length;
-	int	index;
-
-	final_length = 0;
-	index = 0;
-	while (index < size)
-	{
-		final_length += ft_str_length(strings[index]);
-		final_length += sep_length;
-		index++;
-	}
-	final_length -= sep_length;
-	return (final_length);
-}
 
 char	*ft_strjoin(int size, char **strs, char *sep)
 {
-	int		full_length;
-	int		index;
-	char	*string;
-	char	*d;
+	char	*s;
+	int		i;
+	int		j;
+	int		c;
 
-	if (size == 0)
-		return ((char *)malloc(sizeof(char)));
-	full_length = ft_compute_final_length(strs, size, ft_str_length(sep));
-	d = (string = (char *)malloc((full_length + 1) * sizeof(char)));
-	if (!d)
-		return (0);
-	index = 0;
-	while (index < size)
+	s = malloc(sizeof(strs));
+	i = 0;
+	c = 0;
+	while (i < size)
 	{
-		ft_strcpy(d, strs[index]);
-		d += ft_str_length(strs[index]);
-		if (index < size - 1)
+		j = 0;
+		while (strs[i][j] != '\0')
 		{
-			ft_strcpy(d, sep);
-			d += ft_str_length(sep);
+			s[c++] = strs[i][j++];
 		}
-		index++;
+		j = 0;
+		while (sep[j] != '\0' && i != size - 1)
+		{
+			s[c++] = sep[j++];
+		}
+		i++;
 	}
-	*d = '\0';
-	return (string);
+	s[c] = '\0';
+	return (s);
 }
